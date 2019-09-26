3d8f057b57bcff483a1e76a5a5af20335fd4360a
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-09-26 12:10:33 +0300

    Add mocking of stdout in command tests.

--- src/__tests__/commands/requests.spec.ts
+++ src/__tests__/commands/requests.spec.ts
@@ -1,10 +1,25 @@
 import GenerateRequests from "../../commands/generate/requests";
 
 describe("Creating templates command", () => {
+  let processStdoutSpy: jest.SpyInstance;
+  let writtenStdout: string;
+
+  beforeEach(() => {
+    writtenStdout = "";
+    processStdoutSpy = jest.spyOn(process.stdout, "write").mockImplementation(str => {
+      writtenStdout += str;
+      return false;
+    });
+  });
+
+  afterEach(() => {
+    processStdoutSpy.mockReset();
+    processStdoutSpy.mockRestore();
+  });
+
   it("does not throw when given a path to existing openapi yaml", async () => {
-    const spy = jest.spyOn(process.stdout, "write");
     await GenerateRequests.run(["src/__tests__/resources/petstore-templates.yaml"]);
-    expect(spy).toHaveBeenCalled();
+    expect(writtenStdout).toEqual(expect.stringMatching(/^-/));
   });
   it("throws when given a path to non-existing openapi yaml", async () => {
     await expect(GenerateRequests.run(["dfhsdgsd.yaml"])).rejects.toThrow("File not found");
--- src/__tests__/commands/templates.spec.ts
+++ src/__tests__/commands/templates.spec.ts
@@ -1,10 +1,25 @@
 import GenerateTemplates from "../../commands/generate/templates";
 
 describe("Creating templates command", () => {
+  let processStdoutSpy: jest.SpyInstance;
+  let writtenStdout: string;
+
+  beforeEach(() => {
+    writtenStdout = "";
+    processStdoutSpy = jest.spyOn(process.stdout, "write").mockImplementation(str => {
+      writtenStdout += str;
+      return false;
+    });
+  });
+
+  afterEach(() => {
+    processStdoutSpy.mockReset();
+    processStdoutSpy.mockRestore();
+  });
+
   it("does not throw when given a path to existing openapi yaml", async () => {
-    const spy = jest.spyOn(process.stdout, "write");
     await GenerateTemplates.run(["src/__tests__/resources/petstore.yaml"]);
-    expect(spy).toHaveBeenCalled();
+    expect(writtenStdout.length).toBeGreaterThan(0);
   });
   it("throws when given a path to non-existing openapi yaml", async () => {
     await expect(GenerateTemplates.run(["dfhsdgsd.yaml"])).rejects.toThrow("File not found");
