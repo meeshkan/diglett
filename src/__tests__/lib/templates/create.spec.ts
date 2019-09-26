228e0b758ac1522f68b0f74a6d410b050b475341
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-09-26 13:43:45 +0300

    Add failing tests.

--- src/__tests__/lib/templates/create.spec.ts
+++ src/__tests__/lib/templates/create.spec.ts
@@ -6,6 +6,8 @@ import { OpenAPIObject } from "loas3/dist/generated/full";
 
 const OPENAPI_DIR = path.resolve(__dirname, "..", "..", "..", "..", "openapi");
 const PETSTORE_YAML = path.join(OPENAPI_DIR, "petstore.yaml");
+const LINK_EXAMPLE_YAML = path.join(OPENAPI_DIR, "link-example.yaml");
+
 const PETSTORE_TEMPLATES = jsYaml.safeLoad(
   fs.readFileSync(path.join(__dirname, "..", "..", "..", "..", "templates", "petstore-templates.yaml")).toString()
 );
@@ -41,50 +43,70 @@ describe("Extracting request templates", () => {
 
 describe("creating requests from OpenAPI", () => {
   let petstore: OpenAPIObject;
+  let linkExample: OpenAPIObject;
 
   beforeAll(async () => {
     petstore = await readOpenAPI(PETSTORE_YAML);
+    linkExample = await readOpenAPI(LINK_EXAMPLE_YAML);
   });
 
-  it("generates template with three request templates from petstore", () => {
-    const requestsTemplate = generateFrom(petstore);
-    expect(requestsTemplate.templates).toHaveLength(3);
+  describe("for link example", () => {
+    it("produces template with six request templates", () => {
+      const requestsTemplate = generateFrom(linkExample);
+      expect(requestsTemplate.templates).toHaveLength(6);
+    });
+    it("has as fourth request template also query parameters", () => {
+      const requestsTemplate = generateFrom(linkExample);
+      const reqTemplate = requestsTemplate.templates[3];
+      expect(reqTemplate).toBeDefined();
+      const req = reqTemplate.req;
+      expect(req).toHaveProperty("query", { state: "{{ state }}" });
+      const parameters = reqTemplate.parameters;
+      expect(parameters).toHaveProperty("state", { schema: { type: "string", enum: ["open", "merged", "decline"] } });
+    });
   });
 
-  it("generates requests template with no parameters as fixed", () => {
-    const requestsTemplate = generateFrom(petstore);
-    const requestSchema = requestsTemplate.templates[0];
-    const req = requestSchema.req;
-    expect(req).toHaveProperty("host", "petstore.swagger.io");
-    expect(req).toHaveProperty("path", "/v1/pets");
-    expect(req).toHaveProperty("protocol", "http");
-    expect(req).toHaveProperty("method", "get");
-    expect(requestSchema).toHaveProperty("parameters", {});
-  });
+  describe("for petstore", () => {
+    it("generates template with three request templates from petstore", () => {
+      const requestsTemplate = generateFrom(petstore);
+      expect(requestsTemplate.templates).toHaveLength(3);
+    });
 
-  it("generates request with parameters having schema", () => {
-    const requestSchemas = generateFrom(petstore);
-    const requestSchema = requestSchemas.templates[2];
-    const req = requestSchema.req;
-    expect(req).toHaveProperty("host", "petstore.swagger.io");
-    expect(req).toHaveProperty("path", "/v1/pets/{{ petId }}");
-    expect(req).toHaveProperty("protocol", "http");
-    expect(req).toHaveProperty("method", "get");
+    it("generates requests template with no parameters as fixed", () => {
+      const requestsTemplate = generateFrom(petstore);
+      const requestSchema = requestsTemplate.templates[0];
+      const req = requestSchema.req;
+      expect(req).toHaveProperty("host", "petstore.swagger.io");
+      expect(req).toHaveProperty("path", "/v1/pets");
+      expect(req).toHaveProperty("protocol", "http");
+      expect(req).toHaveProperty("method", "get");
+      expect(requestSchema).toHaveProperty("parameters", {});
+    });
 
-    const expectedParameters = {
-      petId: {
-        required: true,
-        schema: {
-          type: "string",
-          pattern: "^\\w+$",
+    it("generates request with parameters having schema", () => {
+      const requestSchemas = generateFrom(petstore);
+      const requestSchema = requestSchemas.templates[2];
+      const req = requestSchema.req;
+      expect(req).toHaveProperty("host", "petstore.swagger.io");
+      expect(req).toHaveProperty("path", "/v1/pets/{{ petId }}");
+      expect(req).toHaveProperty("protocol", "http");
+      expect(req).toHaveProperty("method", "get");
+
+      const expectedParameters = {
+        petId: {
+          required: true,
+          schema: {
+            type: "string",
+            pattern: "^\\w+$",
+          },
         },
-      },
-    };
-    expect(requestSchema).toHaveProperty("parameters", expectedParameters);
-  });
+      };
+      expect(requestSchema).toHaveProperty("parameters", expectedParameters);
+    });
 
-  it("should produce the expected template", () => {
-    const requestSchemas = generateFrom(petstore);
-    expect(requestSchemas).toEqual(PETSTORE_TEMPLATES);
+    it("should produce the expected template", () => {
+      const requestSchemas = generateFrom(petstore);
+      expect(requestSchemas).toEqual(PETSTORE_TEMPLATES);
+    });
   });
 });
