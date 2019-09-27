a4e030b218d009e422e8c653ae8079084bedfc7f
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-09-27 13:36:44 +0300

    Fix test.

--- src/__tests__/lib/bombard/bombard.spec.ts
+++ src/__tests__/lib/bombard/bombard.spec.ts
@@ -1,6 +1,16 @@
 import { bombard } from "../../../lib/bombard";
 import { ISerializedRequest } from "../../../lib/types";
 
+const req: ISerializedRequest = {
+  host: "example.com",
+  method: "get",
+  path: "/v1",
+  pathname: "/v1",
+  query: {},
+  protocol: "https",
+};
+const res = { code: 200 };
+
 describe("Bombard", () => {
   it("works for empty requests file", async () => {
     const result = await bombard([]);
@@ -8,18 +18,10 @@ describe("Bombard", () => {
   });
 
   it("uses the given fake sender", async () => {
-    const req: ISerializedRequest = {
-      host: "example.com",
-      method: "get",
-      path: "/v1",
-      pathname: "/v1",
-      query: {},
-      protocol: "https",
-    };
-    const res = { code: 200 };
-    const sendRequest = jest.fn().mockReturnValue(Promise.resolve(res));
-    const result = await bombard([req], { sendRequest });
-    expect(sendRequest).toHaveBeenCalledWith(req);
+    const sendMock = jest.fn().mockReturnValue(Promise.resolve(res));
+    const result = await bombard([req], { sendRequest: sendMock });
+    expect(sendMock).toHaveBeenCalledTimes(1);
+    expect(sendMock).toHaveBeenCalledWith(req);
     expect(result).toEqual([{ req, res }]);
   });
 });
